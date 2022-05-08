6b31ff5a5cbf92ef1d6d5f62c9f5d880806041a2
eDoZZ <florian.dobkowski@gmail.com>
2022-05-08 19:48:38 +0200

    basic interfaces for the maze and size setter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import { Formik, Form, Field } from "formik";
 
 function App() {
+
+  const [maze, setMaze] = useState([])
+
+  const handleSetter = (values) => {
+    const newMaze = []
+
+    for (let i = 0; i < values.sizeX; i++) {
+
+      newMaze.push([])
+      for (let j = 0; j < values.sizeY; j++) {
+        newMaze[i].push(1)
+      }
+    }
+
+    setMaze(newMaze)
+  }
+
+  useEffect(() => {
+    console.log(typeof maze)
+  }, [maze])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {maze.length !== 0 ?
+        <div className="maze-container">
+          <ul className="maze">
+            {maze.map(x => (
+                <li key={maze.indexOf(x)}>
+                  {x.map(y => (
+                      <p>
+                        {y}
+                      </p>
+                  ))}
+                </li>
+            ))}
+          </ul>
+        </div> :
+        <Formik initialValues={{
+          sizeX: 0,
+          sizeY: 0
+        }} enableReinitialize={true} onSubmit={(values => handleSetter(values))}>
+          <Form>
+            <Field name="sizeY"/>
+            <p>x</p>
+            <Field name="sizeX"/>
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      }
     </div>
   );
 }
